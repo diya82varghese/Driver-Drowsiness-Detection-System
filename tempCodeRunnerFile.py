23f2f0190da279d28567c6864e417d91b19ca10f
diya82varghese <97173675+diya82varghese@users.noreply.github.com>
2024-05-13 08:59:56 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+imSmall = cv2.resize(im, None, 
+                            fx = 1.0/FACE_DOWNSAMPLE_RATIO, 
+                            fy = 1.0/FACE_DOWNSAMPLE_RATIO, 
+                            interpolation = cv2.INTER_LINEAR)
